58c860ea99c5ae8fb886c1a00072a8633e73983e
Ben Huang <tohuangbin@gmail.com>
2018-02-06 15:30:22 +0800

    fix bug about process path;

--- find.js
+++ find.js
@@ -4,13 +4,13 @@ const path = require("path");
 function findPackageJson() {
     let configPath = "./package.json";
     while (! fs.existsSync(configPath)) {
-        const absolutePath = path.resolve(__dirname, configPath);
+        const absolutePath = path.resolve(process.cwd(), configPath);
         if (path.dirname(absolutePath) === "/") {
             throw new Error("can not find package.json !!")
             break;
         }
         configPath = "../" + configPath;
     }
-    return path.resolve(__dirname, configPath);
+    return path.resolve(process.cwd(), configPath);
 }
 module.exports = findPackageJson;
