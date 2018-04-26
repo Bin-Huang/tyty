d35b592fccd548e7f5f6ae5f0191aabe95d36c37
Ben Huang <tohuangbin@gmail.com>
2018-04-26 22:14:05 +0800

    better code style in find.ts

--- src/find.ts
+++ src/find.ts
@@ -2,15 +2,14 @@ import * as fs from 'fs-extra'
 import * as path from 'path'
 
 export default function findPackageJson(): string {
-    let configPath = "./package.json";
-    let i = 0;
-    while (! fs.existsSync(configPath)) {
-        i ++;
-        if (i > 10000) {
-          throw new Error('can not find package.json!');
-          break;
-        }
-        configPath = "../" + configPath;
+  let configPath = "./package.json";
+  let i = 0;
+  while (!fs.existsSync(configPath)) {
+    i++;
+    if (i > 10000) {
+      throw new Error('can not find package.json!');
     }
-    return path.resolve(process.cwd(), configPath);
+    configPath = "../" + configPath;
+  }
+  return path.resolve(process.cwd(), configPath);
 }
